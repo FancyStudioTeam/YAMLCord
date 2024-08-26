72ea4ce72de0ca8fb67e7e052a03f59ed5ecfebd
LonelyDeath-VGX <lonelydeathvgx@gmail.com>
2024-08-26 16:35:24 +0200

    First implementation of createSequences

--- src/util/functions/createSequences.ts
+++ src/util/functions/createSequences.ts
@@ -1,7 +1,26 @@
+import { match } from "ts-pattern";
+import type { Sequence } from "#types";
+import { ErrorCodes } from "#util/errors/ErrorCodes";
+import { FancyScriptError } from "#util/errors/Errors";
 import { loadYAMLData } from "./loadYAMLData";
 
-export const createSequences = async (data: string) => {
-  const loadedData = await loadYAMLData(data);
+export const createSequences = async (
+  data: string,
+): Promise<{
+  sequences: Sequence[];
+}> => {
+  const loadedData = await Object(loadYAMLData(data));
+  const sequences: Sequence[] = [];
 
-  return loadedData;
+  for (const [property] of Object.entries(loadedData)) {
+    match(property)
+      .with("sequences", () => {})
+      .otherwise((property) => {
+        throw new FancyScriptError(ErrorCodes.UNKNOWN_GLOBAL_PROPERTY, property);
+      });
+  }
+
+  return {
+    sequences,
+  };
 };
