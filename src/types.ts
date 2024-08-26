6eea0055b2f09f8f4471a2561ec8cd617ad31bbe
LonelyDeath-VGX <lonelydeathvgx@gmail.com>
2024-08-26 16:44:53 +0200

    Move `SequenceType` to function types

--- src/types.ts
+++ src/types.ts
@@ -1,7 +1,2 @@
 export * from "./util/functions/types";
 export * from "./util/functions/conditionals/types";
-
-export enum SequenceType {
-  CONDITIONAL,
-  FUNCTION,
-}
--- src/util/functions/types.ts
+++ src/util/functions/types.ts
@@ -1,3 +1,8 @@
 import type { Conditional } from "./conditionals/types";
 
+export enum SequenceType {
+  CONDITIONAL,
+  FUNCTION,
+}
+
 export type Sequence = Conditional;
