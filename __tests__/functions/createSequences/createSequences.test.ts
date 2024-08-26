51d2963b0fe2f39c3fcfaf30d3a8389f4e3932af
LonelyDeath-VGX <lonelydeathvgx@gmail.com>
2024-08-26 16:34:32 +0200

    Update `createSequences` tests

--- __tests__/functions/createSequences/createSequences.test.ts
+++ __tests__/functions/createSequences/createSequences.test.ts
@@ -11,43 +11,46 @@ const loader = (path: string) => readFileSync(join(__dirname, "files", `${path}.
 describe("Function: createSequences", () => {
   describe("Success cases (1xxx)", () => {
     it("[1001] Creates valid sequences", async () =>
-      await expect(createSequences(loader("1xxx/Test_1001"))).resolves.toStrictEqual([
-        {
-          sequences: [
-            {
-              type: SequenceType.CONDITIONAL,
-              data: {
-                if: {
-                  operator: "===",
-                  value: "!hello",
-                  variable: "[message.content]",
-                },
-                then: [
-                  {
-                    data: {
-                      content: "Hello, world!",
-                      reply: "no_ping",
-                    },
-                    name: "create_message",
-                    type: SequenceType.FUNCTION,
-                  },
-                  {
-                    data: "👋",
-                    name: "create_reaction",
-                    type: SequenceType.FUNCTION,
-                  },
-                ],
+      await expect(createSequences(loader("1xxx/Test_1001"))).resolves.toStrictEqual({
+        sequences: [
+          {
+            type: SequenceType.CONDITIONAL,
+            data: {
+              if: {
+                operator: "===",
+                value: "!hello",
+                variable: "[message.content]",
               },
+              then: [
+                {
+                  data: {
+                    content: "Hello, world!",
+                    reply: "no_ping",
+                  },
+                  name: "create_message",
+                  type: SequenceType.FUNCTION,
+                },
+                {
+                  data: "👋",
+                  name: "create_reaction",
+                  type: SequenceType.FUNCTION,
+                },
+              ],
             },
-          ],
-        },
-      ]));
+          },
+        ],
+      }));
   });
 
   describe("Failure cases (2xxx)", () => {
     it("[2001] Creates invalid sequences and then rejects with error", async () =>
       await expect(createSequences(loader("2xxx/Test_2001"))).rejects.toThrowError(
         getErrorMessage(ErrorCodes.INVALID_YAML_DATA),
+      ));
+
+    it("[2002] Creates invalid sequences and then rejects with error", async () =>
+      await expect(createSequences(loader("2xxx/Test_2002"))).rejects.toThrowError(
+        getErrorMessage(ErrorCodes.UNKNOWN_GLOBAL_PROPERTY, "conditions"),
       ));
   });
 });
