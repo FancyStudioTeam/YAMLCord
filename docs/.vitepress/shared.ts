afd21fafc1de70415b7c941f735668aafd510a02
LonelyDeath-VGX <lonelydeathvgx@gmail.com>
2024-08-26 22:29:41 +0200

    Update `shared.ts`

--- docs/.vitepress/shared.ts
+++ docs/.vitepress/shared.ts
@@ -1,6 +1,15 @@
 import { defineConfig } from "vitepress";
 
 export const shared = defineConfig({
+  head: [
+    ["link", { rel: "icon", type: "image/png", href: "/Logo.png" }],
+    ["meta", { name: "theme-color", content: "#4cb699" }],
+    ["meta", { property: "og:type", content: "website" }],
+    ["meta", { property: "og:locale", content: "en" }],
+    ["meta", { property: "og:title", content: "FancyScript | A Discord Bot Scripting Sintax" }],
+    ["meta", { property: "og:site_name", content: "FancyScript" }],
+    ["meta", { property: "og:url", content: "https://fancyscript.pages.dev/" }],
+  ],
   themeConfig: {
     logo: "/Logo.png",
     siteTitle: false,
