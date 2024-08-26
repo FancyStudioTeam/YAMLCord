4ffa00f11a8d59e9072adb5e8b4f012d6885fc41
LonelyDeath-VGX <lonelydeathvgx@gmail.com>
2024-08-26 16:34:58 +0200

    Update errors

--- src/util/errors/ErrorMessages.ts
+++ src/util/errors/ErrorMessages.ts
@@ -1,6 +1,7 @@
 import { ErrorCodes } from "./ErrorCodes";
 
-export const ErrorMessages: Record<ErrorCodes, string> = {
-  [ErrorCodes.UNKNOWN_ERROR]: "An unknown error occurred",
+export const ErrorMessages: Record<ErrorCodes, string | ((data: string) => string)> = {
+  [ErrorCodes.GENERAL_ERROR]: "A general error occurred",
   [ErrorCodes.INVALID_YAML_DATA]: "Invalid YAML data",
+  [ErrorCodes.UNKNOWN_GLOBAL_PROPERTY]: (property: string) => `Unknown global property: ${property}`,
 };
--- src/util/errors/Errors.ts
+++ src/util/errors/Errors.ts
@@ -1,13 +1,14 @@
 import type { ErrorCodes } from "./ErrorCodes";
 import { ErrorMessages } from "./ErrorMessages";
 
-export const getErrorMessage = (code: ErrorCodes) => ErrorMessages[code];
+export const getErrorMessage = (code: ErrorCodes, data?: string): string =>
+  typeof ErrorMessages[code] === "function" ? ErrorMessages[code](String(data)) : ErrorMessages[code];
 
 export class FancyScriptError extends Error {
   code: ErrorCodes;
 
-  constructor(code: ErrorCodes) {
-    super(getErrorMessage(code));
+  constructor(code: ErrorCodes, data?: string) {
+    super(getErrorMessage(code, data));
     this.name = `${this.constructor.name} [${code}]`;
     this.code = code;
 
