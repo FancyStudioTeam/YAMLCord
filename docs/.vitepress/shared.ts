86e14303328014b76e928509bd88d24fa5c40e91
LonelyDeath-VGX <lonelydeathvgx@gmail.com>
2024-08-26 22:40:44 +0200

    Format

--- docs/.vitepress/shared.ts
+++ docs/.vitepress/shared.ts
@@ -3,13 +3,56 @@ import { defineConfig } from "vitepress";
 export const shared = defineConfig({
   description: "An easy-to-use syntax for Discord bots using YAML",
   head: [
-    ["link", { rel: "icon", type: "image/png", href: "/Logo.png" }],
-    ["meta", { name: "theme-color", content: "#4cb699" }],
-    ["meta", { property: "og:type", content: "website" }],
-    ["meta", { property: "og:locale", content: "en" }],
-    ["meta", { property: "og:title", content: "FancyScript | An easy-to-use syntax for Discord bots using YAML" }],
-    ["meta", { property: "og:site_name", content: "FancyScript" }],
-    ["meta", { property: "og:url", content: "https://fancyscript.pages.dev/" }],
+    [
+      "link",
+      {
+        rel: "icon",
+        type: "image/png",
+        href: "/Logo.png",
+      },
+    ],
+    [
+      "meta",
+      {
+        name: "theme-color",
+        content: "#4cb699",
+      },
+    ],
+    [
+      "meta",
+      {
+        property: "og:type",
+        content: "website",
+      },
+    ],
+    [
+      "meta",
+      {
+        property: "og:locale",
+        content: "en",
+      },
+    ],
+    [
+      "meta",
+      {
+        property: "og:title",
+        content: "FancyScript | An easy-to-use syntax for Discord bots using YAML",
+      },
+    ],
+    [
+      "meta",
+      {
+        property: "og:site_name",
+        content: "FancyScript",
+      },
+    ],
+    [
+      "meta",
+      {
+        property: "og:url",
+        content: "https://fancyscript.pages.dev/",
+      },
+    ],
   ],
   themeConfig: {
     logo: "/Logo.png",
