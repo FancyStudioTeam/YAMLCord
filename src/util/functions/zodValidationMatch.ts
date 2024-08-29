00996d442506f21235e92b2631700686856943cb
LonelyDeath-VGX <lonelydeathvgx@gmail.com>
2024-08-29 15:10:53 +0200

    `zodValidationMatch` is now synchronous

--- src/util/functions/zodValidationMatch.ts
+++ src/util/functions/zodValidationMatch.ts
@@ -1,36 +1,38 @@
 import { match } from "ts-pattern";
 import type { ZodSchema, z } from "zod";
+import type { ResultErrorType } from "#types";
+import { FancyScriptError } from "#util/errors/Errors";
 import { ErrorCodes } from "../errors/ErrorCodes";
 
-export const zodValidationMatch = async <T extends ZodSchema>(
-  zodSchema: ZodSchema,
-  dataToValidate: unknown,
-): Promise<z.infer<T>> =>
-  new Promise((resolve, reject) => {
-    const { error, data } = zodSchema.safeParse(dataToValidate);
+const throwError = (error: ResultErrorType) => {
+  throw new FancyScriptError(error[0], ...error.slice(1));
+};
 
-    if (error) {
-      match(error.issues[0])
-        .with(
-          {
-            code: "invalid_type",
-          },
-          (issue) => reject([ErrorCodes.INVALID_VALUE_TYPE, issue.expected, issue.received]),
-        )
-        .with(
-          {
-            code: "too_small",
-          },
-          (issue) => reject([ErrorCodes.INVALID_ARRAY_LENGTH, "min", issue.minimum]),
-        )
-        .with(
-          {
-            code: "too_big",
-          },
-          (issue) => reject([ErrorCodes.INVALID_ARRAY_LENGTH, "max", issue.maximum]),
-        )
-        .otherwise(() => reject([ErrorCodes.GENERAL_ERROR]));
-    }
+export const zodValidationMatch = <T extends ZodSchema>(zodSchema: ZodSchema, dataToValidate: unknown): z.infer<T> => {
+  const { error, data } = zodSchema.safeParse(dataToValidate);
 
-    resolve(data);
-  });
+  if (error) {
+    match(error.issues[0])
+      .with(
+        {
+          code: "invalid_type",
+        },
+        (issue) => throwError([ErrorCodes.INVALID_VALUE_TYPE, issue.expected, issue.received]),
+      )
+      .with(
+        {
+          code: "too_small",
+        },
+        (issue) => throwError([ErrorCodes.INVALID_ARRAY_LENGTH, "min", issue.minimum]),
+      )
+      .with(
+        {
+          code: "too_big",
+        },
+        (issue) => throwError([ErrorCodes.INVALID_ARRAY_LENGTH, "max", issue.maximum]),
+      )
+      .otherwise(() => throwError([ErrorCodes.GENERAL_ERROR]));
+  }
+
+  return data;
+};
