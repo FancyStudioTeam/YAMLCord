a55cfe054e27bfedc3b122f8f82c4732579de0d8
LonelyDeath-VGX <lonelydeathvgx@gmail.com>
2024-08-29 17:07:14 +0200

    Add `validateConditional*` methods

---
+++ src/util/functions/conditionals/util/functions/validateConditionalValue.ts
@@ -0,0 +1,1 @@
+export const validateConditionalValue = (value: string) => {};
--- src/util/functions/conditionals/validateConditional.ts
+++ src/util/functions/conditionals/validateConditional.ts
@@ -4,29 +4,32 @@ import { type Conditional, type ResultErrorType, SequenceType } from "#types";
 import { throwError } from "#util/throwError";
 import { zodValidationMatch } from "../zodValidationMatch";
 import { ConditionalSchema } from "./schemas/ConditionalSchemas";
+import { validateConditionalOperator } from "./util/functions/validateConditionalOperator";
+import { validateConditionalValue } from "./util/functions/validateConditionalValue";
+import { validateConditionalVariable } from "./util/functions/validateConditionalVariable";
 
 export const validateConditional = async (conditional: unknown): Promise<Conditional> => {
   const result = await Result.fromAsync<z.infer<ConditionalSchemaType>, ResultErrorType>(() =>
     zodValidationMatch<ConditionalSchemaType>(ConditionalSchema, conditional),
   );
 
-  console.log(result);
-
   if (result.isErr()) {
     throwError(result.unwrapErr());
   }
 
   const data = result.unwrap();
-
-  console.log("DATA:", data);
+  const [rawVariable, rawOperator, ...rawValue] = data.if.split(" ");
+  const variable = validateConditionalOperator(rawVariable);
+  const operator = validateConditionalValue(rawOperator);
+  const value = validateConditionalVariable(rawValue.join(" "));
 
   return {
     type: SequenceType.CONDITIONAL,
     data: {
       if: {
-        operator: "===",
-        value: "!hello",
-        variable: "[message.content]",
+        operator,
+        value,
+        variable,
       },
     },
   };
