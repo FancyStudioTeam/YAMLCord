e286cbd1ded8351d91e20a0710ddeb120f8a7bdb
LonelyDeath-VGX <lonelydeathvgx@gmail.com>
2024-09-05 13:31:50 +0200

    Remove this

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -2,10 +2,8 @@ import "@fontsource/jetbrains-mono/700.css";
 import "@fontsource-variable/plus-jakarta-sans";
 import type { Theme } from "vitepress";
 import DefaultTheme from "vitepress/theme-without-fonts";
-import { h } from "vue";
 import "./style.css";
 
-export default {
+export default (<Theme>{
   extends: DefaultTheme,
-  Layout: () => h(DefaultTheme.Layout, null, {}),
-} satisfies Theme;
+});
