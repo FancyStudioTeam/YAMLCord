830a2eaaf4051250fa939b444e7f3304bede0b78
LonelyDeath-VGX <lonelydeathvgx@gmail.com>
2024-09-05 17:10:50 +0200

    Stuff

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,9 +1,11 @@
 import "@fontsource/jetbrains-mono/700.css";
 import "@fontsource-variable/plus-jakarta-sans";
+import "./style.css";
 import type { Theme } from "vitepress";
 import DefaultTheme from "vitepress/theme-without-fonts";
-import "./style.css";
+import { h } from "vue";
 
 export default (<Theme>{
   extends: DefaultTheme,
+  Layout: () => h(DefaultTheme.Layout, null),
 });
