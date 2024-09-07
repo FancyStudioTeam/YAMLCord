0c42dbbd9e70c59cd3952144d26ba93ce03e9642
LonelyDeath-VGX <lonelydeathvgx@gmail.com>
2024-09-07 13:15:09 +0200

    Improved `AddReaction*` schemas

--- src/util/functions/functions/functions/addReaction/schema.ts
+++ src/util/functions/functions/functions/addReaction/schema.ts
@@ -1,6 +1,11 @@
+import { FormattedCustomEmoji } from "@sapphire/discord-utilities";
+import emojiRegex from "emoji-regex";
 import { z } from "zod";
 
-const AddReactionStringSchema = z.string();
-const AddReactionArraySchema = z.array(AddReactionStringSchema);
+const AddReactionUnicodeEmojiSchema = z.string().regex(emojiRegex());
+const AddReactionCustomEmojiSchema = z.string().regex(FormattedCustomEmoji);
 
-export const AddReactionFunctionSchema = z.union([AddReactionStringSchema, AddReactionArraySchema]);
+const AddReactionStringSchema = z.union([AddReactionUnicodeEmojiSchema, AddReactionCustomEmojiSchema]);
+const AddReactionArraySchema = z.array(AddReactionStringSchema).min(1).max(10);
+
+export const AddReactionSchema = z.union([AddReactionStringSchema, AddReactionArraySchema]);
