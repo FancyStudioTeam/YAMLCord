b54998f93edf184e97fa1cc8acff219bfa08684b
LonelyDeath-VGX <lonelydeathvgx@gmail.com>
2024-09-07 12:14:14 +0200

    Fix this

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,9 +1,9 @@
 import "@fontsource/jetbrains-mono/700.css";
 import "@fontsource-variable/plus-jakarta-sans";
-import "./style.css";
 import type { Theme } from "vitepress";
 import DefaultTheme from "vitepress/theme-without-fonts";
 import { h } from "vue";
+import "./style.css";
 
 export default (<Theme>{
   extends: DefaultTheme,
