6f1facc6484a17ea2f3a8c68a588973258d62c27
LonelyDeath-VGX <lonelydeathvgx@gmail.com>
2024-09-09 19:04:54 +0200

    Use `EMOJI_REGEX`

--- src/functions/functions/add_reaction/schemas/AddReactionSchema.ts
+++ src/functions/functions/add_reaction/schemas/AddReactionSchema.ts
@@ -1,8 +1,13 @@
 import emojiRegex from "emoji-regex";
 import { z } from "zod";
 
-const AddReactionUnicodeEmojiSchema = z.string().regex(emojiRegex());
-const AddReactionCustomEmojiSchema = z.string().regex(/<a?:[a-zA-Z0-9_]+:\d{17,21}>/);
+export const EMOJI_REGEX = {
+  UNICODE: emojiRegex(),
+  CUSTOM: /<a?:[a-zA-Z0-9_]+:\d{17,21}>/,
+};
+
+const AddReactionUnicodeEmojiSchema = z.string().regex(EMOJI_REGEX.UNICODE);
+const AddReactionCustomEmojiSchema = z.string().regex(EMOJI_REGEX.CUSTOM);
 
 const AddReactionStringSchema = z.union([AddReactionUnicodeEmojiSchema, AddReactionCustomEmojiSchema]);
 const AddReactionArraySchema = z.array(AddReactionStringSchema).min(1).max(10);
