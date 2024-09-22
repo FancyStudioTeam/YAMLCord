79d636664d93fd3add8784df1f4999eb9a11d9f9
Hongmeng Zhai <zhaihongmeng@gmail.com>
2024-09-22 15:34:51 -0300

    Added constants to schema maximum values. Changed all instances proxied_url* in MessageEmbedSchema.ts to proxy_url*.

--- src/functions/functions/create_message/schemas/MessageEmbedSchema.ts
+++ src/functions/functions/create_message/schemas/MessageEmbedSchema.ts
@@ -1,5 +1,12 @@
 import { z } from "zod";
 
+// constants
+const MAX_STRING_LENGTH = 256;
+const MAX_DESCRIPTION_LENGTH = 4096;
+const MAX_FOOTER_LENGTH = 2048;
+const MAX_FOOTER_VALUE_LENGTH = 1024;
+const MAX_FIELDS = 25;
+
 // color
 export const HEX_COLOR_REGEX = /^#([0-9a-fA-F]{3}){1,2}$/;
 
@@ -15,19 +22,19 @@ export const MessageEmbedAssetDataSchema = z.object({
   height: z.number().optional(),
   width: z.number().optional(),
   url: MessageEmbedAssetUrlSchema.optional(),
-  proxied_url: MessageEmbedAssetUrlSchema.optional(),
+  proxy_url: MessageEmbedAssetUrlSchema.optional(),
 });
 
 export const MessageEmbedAssetSchema = z.union([MessageEmbedAssetUrlSchema, MessageEmbedAssetDataSchema]).optional();
 
 // author
-export const MessageEmbedAuthorNameSchema = z.string().min(1).max(256);
+export const MessageEmbedAuthorNameSchema = z.string().min(1).max(MAX_STRING_LENGTH);
 
 export const MessageEmbedAuthorDataSchema = z.object({
   name: MessageEmbedAuthorNameSchema,
   url: MessageEmbedAssetUrlSchema.optional(),
   icon_url: MessageEmbedAssetUrlSchema.optional(),
-  proxied_icon_url: z.string().url().optional(),
+  proxy_icon_url: z.string().url().optional(),
 });
 
 export const MessageEmbedAuthorSchema = z
@@ -36,8 +43,8 @@ export const MessageEmbedAuthorSchema = z
 
 // field
 export const MessageEmbedFieldSchema = z.object({
-  name: z.string().min(1).max(256),
-  content: z.string().min(1).max(1024),
+  name: z.string().min(1).max(MAX_STRING_LENGTH),
+  value: z.string().min(1).max(MAX_FOOTER_VALUE_LENGTH),
   inline: z.boolean().optional(),
 });
 
@@ -48,12 +55,12 @@ export const MessageEmbedTimestampSchema = z.union([
 ]);
 
 // footer
-export const MessageEmbedFooterContentSchema = z.string().min(1).max(2048);
+export const MessageEmbedFooterContentSchema = z.string().min(1).max(MAX_FOOTER_LENGTH);
 
 export const MessageEmbedFooterDataSchema = z.object({
   text: MessageEmbedFooterContentSchema,
   icon_url: z.string().url().optional(),
-  proxied_url: z.string().url().optional(),
+  proxy_icon_url: z.string().url().optional(),
 });
 
 export const MessageEmbedFooterSchema = z
@@ -63,14 +70,16 @@ export const MessageEmbedFooterSchema = z
 // embed
 export const MessageEmbedSchema = z.object({
   color: MessageEmbedHexColorSchema.optional(),
-  title: z.string().min(1).max(256).optional(),
+  title: z.string().min(1).max(MAX_STRING_LENGTH).optional(),
   author: MessageEmbedAuthorSchema,
   url: z.string().url().optional(),
-  description: z.string().min(1).max(4096).optional(),
+  description: z.string().min(1).max(MAX_DESCRIPTION_LENGTH).optional(),
   thumbnail: MessageEmbedAssetSchema,
-  fields: z.array(MessageEmbedFieldSchema).min(1).max(25).optional(),
+  fields: z.array(MessageEmbedFieldSchema).min(1).max(MAX_FIELDS).optional(),
   image: MessageEmbedAssetSchema,
   video: MessageEmbedAssetSchema,
   timestamp: MessageEmbedTimestampSchema.optional(),
   footer: MessageEmbedFooterSchema,
 });
+
+export type MessageEmbed = z.infer<typeof MessageEmbedSchema>;
