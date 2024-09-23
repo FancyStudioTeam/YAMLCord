d41b9f24d8be4dba05491e4519f64c3ff66c9fc1
LonelyDeath-VGX <lonelydeathvgx@gmail.com>
2024-09-23 21:04:01 +0200

    Update exports

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,2 @@
-import { type Conditional, type Function, type Sequence, SequenceType } from "#types";
-
 export { YAMLCord } from "./classes/YAMLCord";
-export { SequenceType } from "./sequences/types";
-
-export const isConditional = (sequence: Sequence): sequence is Conditional =>
-  sequence.type === SequenceType.CONDITIONAL;
-export const isFunction = (sequence: Sequence): sequence is Function => sequence.type === SequenceType.FUNCTION;
+export * from "#types";
