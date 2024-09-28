d6a6dda77b7de3313d9e1cf274d77280c2a66844
LonelyDeath-VGX <lonelydeathvgx@gmail.com>
2024-09-28 20:34:11 +0200

    Fix import

--- src/conditionals/util/validateConditionalOperator.ts
+++ src/conditionals/util/validateConditionalOperator.ts
@@ -1,11 +1,11 @@
 import { ConditionalIfOperatorSchema } from "#conditionals/schemas/ConditionalSchemas.js";
 import { throwError } from "#errors";
-import type { ConditionalIfOperator } from "#types";
+import type { ResolvedConditionalIfOperator } from "#types";
 import { zod } from "#zod";
 
 export const validateConditionalOperator = async (operator: unknown) => {
   const data = await zod(ConditionalIfOperatorSchema, operator).catch((error) => throwError(error));
-  const operatorMap: Record<typeof data, ConditionalIfOperator> = {
+  const operatorMap: Record<typeof data, ResolvedConditionalIfOperator> = {
     eq: "===",
     neq: "!==",
   };
