36f07ac271ad2fdab403f70fd7128c7be9bafa64
LonelyDeath-VGX <lonelydeathvgx@gmail.com>
2024-09-28 19:50:26 +0200

    Allow only enums to variables

--- __tests__/conditionals/variable/variable.test.ts
+++ __tests__/conditionals/variable/variable.test.ts
@@ -5,41 +5,42 @@ import { getErrorMessage } from "../../../src/util/errors/Errors.js";
 
 describe("YAMLCord Conditional Structure: variable", () => {
   describe("Success cases (1xxx)", () => {
-    it("[1001]", async () => await expect(validateConditionalVariable("[data]")).resolves.toStrictEqual("[data]"));
+    it("[1001]", async () =>
+      await expect(validateConditionalVariable("[message_content]")).resolves.toStrictEqual("[message_content]"));
 
     it("[1002]", async () =>
-      await expect(validateConditionalVariable("[message.content]")).resolves.toStrictEqual("[message.content]"));
+      await expect(validateConditionalVariable("[date_now]")).resolves.toStrictEqual("[date_now]"));
   });
 
   describe("Failure cases (2xxx)", () => {
     it("[2001]", async () =>
       await expect(validateConditionalVariable("[data")).rejects.toThrowError(
-        getErrorMessage(ErrorCodes.INVALID_STRING_REGEX),
+        getErrorMessage(ErrorCodes.INVALID_ENUM_VALUE),
       ));
 
     it("[2002]", async () =>
       await expect(validateConditionalVariable("data")).rejects.toThrowError(
-        getErrorMessage(ErrorCodes.INVALID_STRING_REGEX),
+        getErrorMessage(ErrorCodes.INVALID_ENUM_VALUE),
       ));
 
     it("[2003]", async () =>
       await expect(validateConditionalVariable("data]")).rejects.toThrowError(
-        getErrorMessage(ErrorCodes.INVALID_STRING_REGEX),
+        getErrorMessage(ErrorCodes.INVALID_ENUM_VALUE),
       ));
 
     it("[2004]", async () =>
       await expect(validateConditionalVariable("message.content")).rejects.toThrowError(
-        getErrorMessage(ErrorCodes.INVALID_STRING_REGEX),
+        getErrorMessage(ErrorCodes.INVALID_ENUM_VALUE),
       ));
 
     it("[2005]", async () =>
       await expect(validateConditionalVariable("[message.[content]")).rejects.toThrowError(
-        getErrorMessage(ErrorCodes.INVALID_STRING_REGEX),
+        getErrorMessage(ErrorCodes.INVALID_ENUM_VALUE),
       ));
 
     it("[2006]", async () =>
       await expect(validateConditionalVariable("[[message.content]]")).rejects.toThrowError(
-        getErrorMessage(ErrorCodes.INVALID_STRING_REGEX),
+        getErrorMessage(ErrorCodes.INVALID_ENUM_VALUE),
       ));
   });
 });
--- src/conditionals/schemas/ConditionalSchemas.ts
+++ src/conditionals/schemas/ConditionalSchemas.ts
@@ -6,7 +6,16 @@ export const ConditionalSchema = z.object({
   else: z.array(z.unknown()).optional(),
 });
 
-export const ConditionalIfVariableSchema = z.string().regex(/^\[[^[\]]+\]$/);
+export const ConditionalIfVariableSchema = z.enum([
+  "[date_now]",
+  "[guild_id]",
+  "[guild_name]",
+  "[message_content]",
+  "[owner_id]",
+  "[owner_name]",
+  "[user_id]",
+  "[user_name]",
+]);
 export const ConditionalIfOperatorSchema = z.enum(["eq", "neq"]);
 export const ConditionalIfValueSchema = z
   .string()
