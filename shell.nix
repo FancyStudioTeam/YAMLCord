a65ceb1474df3423b16522e2175a7b0ca439862f
hozhai <zhaihongmeng@gmail.com>
2024-09-29 14:57:27 -0300

    Improve Biome script in package.json

--- shell.nix
+++ shell.nix
@@ -10,7 +10,7 @@
   shellHook = ''
     export NODE_ENV=development
     export IN_NIX_SHELL=1
-    export BIOME_BIN=$(which biome)
+    export BIOME_BIN_PATH=$(which biome)
     echo "You are now ready for development! Yay!"
   '';
 }
