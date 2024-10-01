c6bff471f5cb818c7a44655529edb76534536722
LonelyDeath-VGX <lonelydeathvgx@gmail.com>
2024-10-01 22:09:47 +0200

    Add `tsc-alias`

--- src/types.ts
+++ src/types.ts
@@ -4,5 +4,6 @@ export type ResultErrorType = [ErrorCodes, ...unknown[]];
 
 export * from "@conditionals/types.js";
 export * from "@functions/types.js";
+export * from "@index";
 export * from "@sequences/types.js";
 export * from "@variables/types.js";
