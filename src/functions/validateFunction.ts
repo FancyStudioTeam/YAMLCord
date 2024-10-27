efe6752cd4376b5ee69396dc366454ceeacca59e
LonelyDeath-VGX <lonelydeathvgx@gmail.com>
2024-10-27 19:43:06 +0100

    Use regex

--- src/functions/validateFunction.ts
+++ src/functions/validateFunction.ts
@@ -2,6 +2,7 @@ import { ErrorCodes, throwError } from "@errors";
 import { SequenceType, type YAMLCordFunction } from "@types";
 import { match } from "ts-pattern";
 
+const FUNCTION_NAME_REGEX = /^[^\s()]+\(\)$/;
 const isValid = (sequence: unknown): sequence is YAMLCordFunction =>
   typeof sequence === "object" && sequence !== null && "type" in sequence && sequence.type === SequenceType.FUNCTION;
 
@@ -9,23 +10,31 @@ export const validateFunction = async (sequence: unknown): Promise<YAMLCordFunct
   let validFunction: YAMLCordFunction | unknown;
 
   for (const [key, value] of Object.entries(new Object(sequence))) {
-    await match(key)
+    const functionNameMatch = key.match(FUNCTION_NAME_REGEX);
+
+    if (!functionNameMatch) {
+      return throwError([ErrorCodes.UNKNOWN_GLOBAL_FUNCTION, key]);
+    }
+
+    const functionName = functionNameMatch[0].toLowerCase().slice(0, -2);
+
+    await match(functionName)
       .with(
-        "add_reaction()",
+        "add_reaction",
         async () =>
           (validFunction = await import("./functions/add_reaction/validateAddReactionFunction.js").then(
             ({ validateAddReactionFunction }) => validateAddReactionFunction(value),
           )),
       )
       .with(
-        "create_message()",
+        "create_message",
         async () =>
           (validFunction = await import("./functions/create_message/validateCreateMessageFunction.js").then(
             ({ validateCreateMessageFunction }) => validateCreateMessageFunction(value),
           )),
       )
       .with(
-        "wait()",
+        "wait",
         async () =>
           (validFunction = await import("./functions/wait/validateWaitFunction.js").then(({ validateWaitFunction }) =>
             validateWaitFunction(value),
